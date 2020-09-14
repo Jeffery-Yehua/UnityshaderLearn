7d158604551de07703ee84d826aef9468d4312f4
Jeffery <ycai0039@student.monash.edu>
2020-09-14 22:22:58 +0800

    Create setfloat.cs
    屏幕空间颜色条移动脚本

---
+++ setfloat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setfloat : MonoBehaviour
+{
+    private float dis=-1;
+    private float r=0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dis+= Time.deltaTime*0.1f;
+        GetComponent<Renderer> ().material.setfloat("dis",dis);
+        GetComponent<Renderer> ().material.setfloat("r",r);
+
+
+        
+    }
+}
